87ec956ab3b0dbf6a43a39debb3592afc314929a
12929 <12929@LAPTOP-AM4199GA>
2019-09-15 17:35:21 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("你好， GitHub");
+		System.out.println("你好， GitHub" + "随便改一下 ! ");
 	}
 
 }
